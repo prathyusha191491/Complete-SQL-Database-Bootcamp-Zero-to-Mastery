b2b43a6e3cdef0e6b2f26b5bbf7c2998a1bcfce9
prathyusha191491 <54631809+prathyusha191491@users.noreply.github.com>
2023-07-23 21:58:57 +0530

    Update questions.sql

--- SQL Deep Dive/IN/questions.sql
+++ SQL Deep Dive/IN/questions.sql
@@ -4,7 +4,7 @@
 * Question: How many orders were made by customer 7888, 1082, 12808, 9623
 */
 
-SELECT * FROM orders;
+SELECT * FROM orders where id in (7888, 1082, 12808, 9623);
 
 
 /*
@@ -13,4 +13,4 @@ SELECT * FROM orders;
 * Question: How many cities are in the district of Zuid-Holland, Noord-Brabant and Utrecht?
 */
 
-SELECT * FROM city;
+SELECT count(*) FROM city where district in ('Zuid-Holland', 'Noord-Brabant','Utrecht');
