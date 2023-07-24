f7822e529a624c00f555a6348ca15c3e11819049
prathyusha191491 <54631809+prathyusha191491@users.noreply.github.com>
2023-07-24 14:54:42 +0530

    Update questions.sql

--- SQL Deep Dive/Date Filtering/questions.sql
+++ SQL Deep Dive/Date Filtering/questions.sql
@@ -4,23 +4,25 @@
 * Question: Get me all the employees above 60, use the appropriate date functions
 */
 
-SELECT * FROM employees;
+SELECT * FROM employees where age(now()-birth_date)>60;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: How many employees where hired in February?
 */
+select count(id) from employees where extract(month from hire_date)=2
 
-SELECT * FROM employees;
+-- SELECT * FROM employees;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: How many employees were born in november?
 */
+select count(id) from employees where extract(month from birth_date)=11
 
-SELECT * FROM employees;
+-- SELECT * FROM employees;
 
 /*
 * DB: Employees
@@ -29,6 +31,8 @@ SELECT * FROM employees;
 */
 
 SELECT * FROM employees;
+select max(age(birth_date)) from employees ;
+
 
 /*
 * DB: Store
@@ -37,4 +41,5 @@ SELECT * FROM employees;
 */
 
 SELECT * FROM orders;
+select count(orderid) from orders where extract(month from order_date)= 1 and  extract(year from order_date)= 2004
 
